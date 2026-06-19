964a4d84a342c1dc57f6227e7a19c2b913a2315c
mhykellupac <mhykellupac78@gmail.com>
2026-06-19 17:12:48 +0800

    Add files via upload

---
+++ dji/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ApusFly DJI Cloud</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+    <h2>ApusFly DJI Cloud System</h2>
+
+    <div id="status">Initializing...</div>
+
+    <div class="footer">
+        DJI Bridge Status Monitor
+    </div>
+</div>
+
+<script src="js/config.js"></script>
+<script src="js/app.js"></script>
+
+</body>
+</html>
